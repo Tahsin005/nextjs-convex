4e98a7f3382812106c273c8193b8e86889477d3e
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-07-18 17:55:37 +0600

    feat: implement email sign-in logic using authClient in login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -16,11 +16,13 @@ import {
     FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 import { Controller, useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
 export default function LoginPage() {
@@ -35,9 +37,24 @@ export default function LoginPage() {
         },
     });
 
-    function onSubmit(data: z.infer<typeof loginSchema>) {
-
+    async function onSubmit(data: z.infer<typeof loginSchema>) {
+        startTransition(async () => {
+            await authClient.signIn.email({
+                email: data.email,
+                password: data.password,
+                fetchOptions: {
+                    onSuccess: () => {
+                        toast.success("Logged in successfully");
+                        router.push("/");
+                    },
+                    onError: (error) => {
+                        toast.error(error.error.message);
+                    },
+                },
+            });
+        });
     }
+
     return (
         <Card>
             <CardHeader>
@@ -85,7 +102,7 @@ export default function LoginPage() {
                             )}
                         />
 
-                        <Button disabled={isPending}>
+                        <Button type="submit" disabled={isPending}>
                             {isPending ? (
                                 <>
                                     <Loader2 className="size-4 animate-spin" />
